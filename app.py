3a6b2b564792dae2875dad8cdbb738a3d54b8d08
Piotr Dziuba <piotr.dziuba@posti.com>
2019-06-24 18:40:46 +0200

    add measurement creating route

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask,jsonify,request
+from flask import Flask,jsonify,request,url_for
 
 app = Flask(__name__)
 
 # GET measurements data
 @app.route('/api/v1/measurements')
 def get_measurements():
+    request_data = request.get_json()
     return jsonify({'measurements': []})
 
+@app.route('/api/v1/measurements/<int:id>', methods=['GET'])
+def get_measurement(id):
+    return jsonify({})
+
+@app.route('/api/v1/measurements', methods=['POST'])
+def create_measurement():
+    request_data = request.get_json()
+    print("create_measurement:", request_data)
+    return jsonify(request_data), 201, {"Location": url_for('get_measurement', id=1)}
+
 if __name__ == "__main__":
+    print(app.url_map)
     app.run(port=5000)
--- test_app.py
+++ test_app.py
@@ -6,6 +6,16 @@ def test_app():
     app.testing = True
     yield app.test_client()
 
-def test_measurements_get(test_app):
+def test_get_measurements(test_app):
     rv = test_app.get('/api/v1/measurements')
     assert rv.status_code == 200
+    assert rv.mimetype == 'application/json'
+
+def test_create_measurement(test_app):
+    data = {
+        "sensor": "test",
+        "value": "10.00",
+        "timestamp": "2019-06-23 12:38"}
+    rv = test_app.post('/api/v1/measurements', json=data)
+    assert rv.status_code == 201
+    assert "/api/v1/measurements/" in rv.headers['Location']
